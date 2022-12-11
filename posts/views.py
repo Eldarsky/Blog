036d1b5309b83a57f1b07f8ff8ce28f4643df754
Eldarsky <nfxat1@mail.ru>
2022-12-11 22:34:37 +0600

    homework 1

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+import datetime
+
+from django.shortcuts import HttpResponse
+
+
+# Create your views here.
+def main(request):
+    return  HttpResponse('Hello! Its my project')
+
+def data(request):
+    return HttpResponse(datetime.datetime.now())
+
+def goodby(request):
+    return HttpResponse('Goodby user!')
+
